0dce858b25f86579416a6bf4a1304f47bbfe368e
Anudeep Reddy Katta <kattaanudeepreddy@gmail.com>
2022-09-11 11:55:37 +0000

    droped mnist realted files

--- tests/test_cifar10_datamodule.py
+++ tests/test_cifar10_datamodule.py
@@ -3,19 +3,19 @@ from pathlib import Path
 import pytest
 import torch
 
-from src.datamodules.mnist_datamodule import MNISTDataModule
+from src.datamodules.cifar10_datamodule import CIFAR10DataModule
 
 
 @pytest.mark.parametrize("batch_size", [32, 128])
-def test_mnist_datamodule(batch_size):
+def test_cifar10_datamodule(batch_size):
     data_dir = "data/"
 
-    dm = MNISTDataModule(data_dir=data_dir, batch_size=batch_size)
+    dm = CIFAR10DataModule(data_dir=data_dir, batch_size=batch_size)
     dm.prepare_data()
 
     assert not dm.data_train and not dm.data_val and not dm.data_test
-    assert Path(data_dir, "MNIST").exists()
-    assert Path(data_dir, "MNIST", "raw").exists()
+    assert Path(data_dir, "CIFAR10").exists()
+    assert Path(data_dir, "CIFAR10", "raw").exists()
 
     dm.setup()
     assert dm.data_train and dm.data_val and dm.data_test
